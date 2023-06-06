6efbaae4645c7e8b09975d81f245a1e732035c69
e.minetti <e.minetti@posteo.de>
2023-06-07 01:09:38 +0200

    Completed section 5.3.8.1

--- src/configuration.rs
+++ src/configuration.rs
@@ -46,8 +46,7 @@ pub struct ApplicationSettings {
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    let base_path = std::env::current_dir()
-        .expect("Failed to determine the current directory");
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
     let configuration_directory = base_path.join("configuration");
 
     let environment: Environment = std::env::var("APP_ENVIRONMENT")
@@ -57,12 +56,12 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let environment_filename = format!("{}.yaml", environment.as_str());
 
     let settings = config::Config::builder()
-        .add_source(
-            config::File::from(configuration_directory.join("base.yaml"))
-        )
-        .add_source(
-            config::File::from(configuration_directory.join(environment_filename))
-        )
+        .add_source(config::File::from(
+            configuration_directory.join("base.yaml"),
+        ))
+        .add_source(config::File::from(
+            configuration_directory.join(environment_filename),
+        ))
         .build()?;
 
     settings.try_deserialize::<Settings>()
@@ -89,11 +88,11 @@ impl TryFrom<String> for Environment {
         match s.to_lowercase().as_str() {
             "local" => Ok(Self::Local),
             "production" => Ok(Self::Production),
-            other=> Err(format!(
+            other => Err(format!(
                 "{} is not a supported environment. \
                 Use either `local` or `production`.",
                 other
-            ))
+            )),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use secrecy::ExposeSecret;
+use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
-use sqlx::postgres::PgPoolOptions;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry;
@@ -15,7 +15,7 @@ async fn main() -> Result<(), std::io::Error> {
     let connection_pool = PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
         .connect_lazy(configuration.database.connection_string().expose_secret())
-            .expect("Failed to connect to Postgres");
+        .expect("Failed to connect to Postgres");
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port

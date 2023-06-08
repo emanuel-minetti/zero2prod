054528417c7cff5820a35031c8701506b90205da
e.minetti <e.minetti@posteo.de>
2023-06-08 22:04:46 +0200

    Repaired section 5.4.6

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,8 +1,8 @@
 use secrecy::ExposeSecret;
 use secrecy::Secret;
 use serde_aux::field_attributes::deserialize_number_from_string;
-use sqlx::ConnectOptions;
 use sqlx::postgres::{PgConnectOptions, PgSslMode};
+use sqlx::ConnectOptions;
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
@@ -31,14 +31,14 @@ impl DatabaseSettings {
         PgConnectOptions::new()
             .host(&self.host)
             .username(&self.username)
-            .password(&self.password.expose_secret())
+            .password(self.password.expose_secret())
             .port(self.port)
             .ssl_mode(ssl_mode)
     }
 
     pub fn with_db(&self) -> PgConnectOptions {
         let mut options = self.without_db().database(&self.database_name);
-        options.log_slow_statements(tracing_log::log::LevelFilter::Trace);
+        options.log_statements(tracing_log::log::LevelFilter::Trace);
         options
     }
 }
@@ -73,7 +73,7 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
         .add_source(
             config::Environment::with_prefix("APP")
                 .prefix_separator("_")
-                .separator("__")
+                .separator("__"),
         )
         .build()?;
 

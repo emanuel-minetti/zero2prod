7cdc5e08710c0f8e50f34cde13a4577b93b94dd0
e.minetti <e.minetti@posteo.de>
2023-07-01 23:11:22 +0200

    Finished  10.6.4.13.

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,3 +1,4 @@
+use actix_web::cookie::Cookie;
 use actix_web::http::header::ContentType;
 use actix_web::{HttpRequest, HttpResponse};
 
@@ -8,7 +9,7 @@ pub async fn login_form(request: HttpRequest) -> HttpResponse {
             format!("<p><i>{}</i></p>", cookie.value())
         }
     };
-    HttpResponse::Ok()
+    let mut response = HttpResponse::Ok()
         .content_type(ContentType::html())
         .body(format!(
             r#"
@@ -42,5 +43,9 @@ pub async fn login_form(request: HttpRequest) -> HttpResponse {
 </body>
 </html>
             "#
-        ))
+        ));
+    response
+        .add_removal_cookie(&Cookie::new("_flash", ""))
+        .unwrap();
+    response
 }

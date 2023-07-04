3921e31319e6a2a506b0c8ee7b30315495e8f4e1
e.minetti <e.minetti@posteo.de>
2023-07-05 00:26:16 +0200

    Finished  10.8.

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -67,5 +67,9 @@ pub async fn change_password(
         return Ok(see_other("/admin/password"));
     }
 
-    todo!()
+    crate::authentication::change_password(user_id, form.0.new_password, &pool)
+        .await
+        .map_err(e500)?;
+    FlashMessage::info("Your password has been changed.").send();
+    Ok(see_other("/admin/password"))
 }
